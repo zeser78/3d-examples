321ab735c89278574aa1fbeb6b700dceff82a6f9
Sergio <solivares@xpaso.com>
2020-10-16 12:27:35 -0400

    adding babylonjs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import ring from './models/ring-shader.glb'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 	var scene = new THREE.Scene();
 			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -9,17 +10,14 @@ let canvas = document.getElementById('glcanvas')
 			var renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 			scene.background = new THREE.Color('white' );
-			// document.body.appendChild( renderer.domElement );
 
-			// var geometry = new THREE.BoxGeometry();
-			// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			// var cube = new THREE.Mesh( geometry, material );
-// scene.add(cube);
-			
-//
+			var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+const controls = new OrbitControls(camera, renderer.domElement);
 
+light.position.z = 6
 camera.position.z = 6;
-			
+			controls.update()
 // GLTFLoader
 
 const loader = new GLTFLoader();
@@ -38,7 +36,7 @@ loader.load(ring, function (gltf) {
 
 				// cube.rotation.x += 0.01;
 				// cube.rotation.y += 0.01;
-
+				controls.update()
 				renderer.render( scene, camera );
 			};
 
